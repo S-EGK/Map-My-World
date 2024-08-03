e6cb4f13d2f956006fe28c029c8db329f11929d2
S-EGK <elkoorsh@mail.uc.edu>
2024-08-02 20:30:48 -0700

    Initial commit

---
+++ build/main/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/segk/map_my_world/src/main
+# Build directory: /home/segk/map_my_world/build/main
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
